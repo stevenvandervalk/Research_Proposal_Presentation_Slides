2e725f58563d89cbe162665762c560c7313dd8ff
Steven Vandervalk <steven.vandervalk@gmail.com>
2014-03-19 13:57:09 +1000

    initial dump

---
+++ slide_config.js
@@ -0,0 +1,40 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'CP 5080 Research Proposal',
+    subtitle: 'Semantic data mining of sensor networks from tropical workplaces',
+    eventInfo: {
+    title: 'Honours 2014',
+    date: '28/03/2014'
+    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/jcu_logo@trans.svg.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['phone'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Steve Vandervalk',
+    company: 'Postgraduate<br>James Cook University',
+    gplus: 'http://plus.google.com/+SteveVandervalk',
+    twitter: '@stevevandervalk',
+    www: 'stevevandervalk.com',
+    github: 'http://github.com/stevenvandervalk'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
